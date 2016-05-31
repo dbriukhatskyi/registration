ff17b198291bab1cdd5660892aec1637611f61be
Dmytro Briukhatsky <bryukhatsky@gmail.com>
2016-05-31 15:39:26 +0300

    Add User entity and UserValidationResult class

--- src/main/java/com/redeyes/registration/controller/RegistrationController.java
+++ src/main/java/com/redeyes/registration/controller/RegistrationController.java
@@ -1,17 +1,21 @@
 package com.redeyes.registration.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.redeyes.registration.model.User;
+import com.redeyes.registration.model.UserValidationResult;
+
 
 
 /**
@@ -36,42 +40,26 @@ public class RegistrationController {
     @RequestMapping(method = RequestMethod.GET)
     public final ModelAndView home() {
         LOG.debug("Returning initial view.");
-        return new ModelAndView("registration", "start", true);
+        return getModelAndView();
     }
 
     /**
      *
+     * @param bindingResult
+     *        model
      *
-     * @param path
-     *        path to a local directory with photos
+     * @param user
+     *        user registration data
      *
-     * @return view
+     * @return ajax
      */
-    @RequestMapping(method = RequestMethod.POST)
-    public final ModelAndView post(@RequestParam final String path) {
-        ModelAndView model = getModelAndView();
-
-        return model;
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public final UserValidationResult post(@ModelAttribute @Valid final User user,
+            final BindingResult bindingResult) {
+        LOG.info("email: '{}', password: '{}'", user.getEmail(), user.getPassword());
+        return new UserValidationResult(bindingResult);
     }
-
-    /**
-    *
-    * @param email
-    *        e-mail to register
-    *
-    * @param password
-    *        user password
-    *
-    * @return view
-    */
-   @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
-   @ResponseBody
-   public final Map<String, String> create(@RequestParam final String email, @RequestParam final String password) {
-       Map<String, String> result = new HashMap<>();
-       result.put("email", email);
-       result.put("password", password);
-       return result;
-   }
 
     /**
      *
@@ -94,6 +82,7 @@ public class RegistrationController {
      */
     private ModelAndView getModelAndView() {
         ModelAndView modelAndView = new ModelAndView("registration");
+        modelAndView.addObject("data", new User());
         return modelAndView;
     }
 }
