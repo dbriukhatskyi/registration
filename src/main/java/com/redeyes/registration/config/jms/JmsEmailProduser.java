f2056ec93fd6d55a878077a57d313247dfae363e
OleksandrDres <oleksandr.dres@gmail.com>
2016-06-02 13:26:33 +0300

    Small refactor

--- src/main/java/com/redeyes/registration/config/jms/JmsEmailProduser.java
+++ src/main/java/com/redeyes/registration/config/jms/JmsEmailProduser.java
@@ -3,9 +3,6 @@ package com.redeyes.registration.config.jms;
 import com.redeyes.registration.model.Email;
 import org.springframework.jms.core.support.JmsGatewaySupport;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Custom jms message sender.
  */
@@ -16,10 +13,6 @@ public class JmsEmailProduser extends JmsGatewaySupport {
      * @param email Email to send.
      */
     public void sendEmail(final Email email) {
-        Map<String, String> map = new HashMap<>();
-        map.put("recipient", email.getRecipient());
-        map.put("subject", email.getSubject());
-        map.put("emailText", email.getEmailText());
         getJmsTemplate().convertAndSend(email);
     }
 }
