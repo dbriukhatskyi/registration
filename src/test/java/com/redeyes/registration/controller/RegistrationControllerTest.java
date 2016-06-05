d1a80fe314c0588845755cf800cf781c50e036b2
OleksandrDres <oleksandr.dres@gmail.com>
2016-06-05 21:06:40 +0300

    Add tests

--- src/test/java/com/redeyes/registration/controller/RegistrationControllerTest.java
+++ src/test/java/com/redeyes/registration/controller/RegistrationControllerTest.java
@@ -1,10 +1,10 @@
 package com.redeyes.registration.controller;
 
-import com.redeyes.registration.model.UserTo;
 import org.junit.Test;
 
 import static com.redeyes.registration.controller.RegistrationController.REGISTRATION_URI;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -19,16 +19,31 @@ public class RegistrationControllerTest extends AbstractControllerTest {
 
     @Test
     public void testPost() throws Exception {
-        UserTo userTo = new UserTo();
-        userTo.setEmail("user@gmail.com");
-        userTo.setPassword("12345");
-
-//        mockMvc.perform(post(REGISTRATION_URI, userTo));
+        mockMvc.perform(post(REGISTRATION_URI)
+                .param("email", "user@gmail.com")
+                .param("password", "12345"))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testUniqueEmail() {
-
+    public void testUniqueEmail() throws Exception {
+        mockMvc.perform(post(REGISTRATION_URI)
+                .param("email", "user@gmail.com")
+                .param("password", "12345"));
     }
 
+    @Test
+    public void testPostEmptyUser() throws Exception {
+        mockMvc.perform(post(REGISTRATION_URI)
+                .param("email", "")
+                .param("password", ""));
+    }
+
+    @Test
+    public void testPostInvalidEmail() throws Exception {
+        mockMvc.perform(post(REGISTRATION_URI)
+                .param("email", "usergmail.com")
+                .param("password", "12345"))
+                .andExpect(status().isOk());
+    }
 }
